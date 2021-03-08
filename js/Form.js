697af8e974aa9a571f69f2a33485f166a0c65c69
Rajgardade <74846908+Rajgardade@users.noreply.github.com>
2021-03-08 20:46:32 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,45 @@
+class Form {
+
+  constructor()
+   {
+    this.input = createInput("Name");
+    this.button = createButton('PLAY');
+    this.greeting = createElement('h2');
+  }
+  hide()
+  {
+     this.input.hide()
+     this.button.hide()
+     this.greeting.hide()
+
+
+
+  }
+
+  display()
+  {
+    var title = createElement('h2')
+    title.html("Car Racing Game");
+    title.position(displayWidth/2-60, 0);
+
+    this.input.position(displayWidth/2-60, 100);
+    this.button.position(displayWidth/2, 200);
+
+    this.button.mousePressed(()=>{
+
+      this.input.hide();
+      this.button.hide();
+      
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+
+      player.update();
+      player.updateCount(playerCount);
+     
+      this.greeting.html("Hello " + player.name )
+      this.greeting.position(displayWidth/2-80, 100);
+    });
+
+  }
+}
